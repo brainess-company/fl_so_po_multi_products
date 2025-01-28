d8c89216d6263ddb9c6a1b4ea9f2fff2c7b55345
BrainessCompany <brainesscompany@gmail.com>
2025-01-28 12:38:26 -0300

    teste de incluir quantidades

--- wizard/select_products_wizard.py
+++ wizard/select_products_wizard.py
@@ -1,40 +1,48 @@
-# -*- coding: utf-8 -*-
 from datetime import datetime
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo import models, fields, api
 
 
 class SelectProducts(models.TransientModel):
-
     _name = 'select.products'
     _description = 'Select Products'
 
-    product_ids = fields.Many2many('product.product', string='Products')
+    product_lines = fields.One2many('select.products.line', 'wizard_id', string='Products')
     flag_order = fields.Char('Flag Order')
 
     def select_products(self):
         if self.flag_order == 'so':
             order_id = self.env['sale.order'].browse(self._context.get('active_id', False))
-            for product in self.product_ids:
+            for line in self.product_lines:
                 self.env['sale.order.line'].create({
-                    'product_id': product.id,
-                    'product_uom': product.uom_id.id,
-                    'price_unit': product.lst_price,
+                    'product_id': line.product_id.id,
+                    'product_uom': line.product_id.uom_id.id,
+                    'price_unit': line.product_id.lst_price,
+                    'product_uom_qty': line.product_qty,
                     'order_id': order_id.id
                 })
         elif self.flag_order == 'po':
             order_id = self.env['purchase.order'].browse(self._context.get('active_id', False))
-            for product in self.product_ids:
-                product_name = product.display_name
-                if product.description_purchase:
-                    product_name += '\n' + product.description_purchase
+            for line in self.product_lines:
+                product_name = line.product_id.display_name
+                if line.product_id.description_purchase:
+                    product_name += '\n' + line.product_id.description_purchase
                 self.env['purchase.order.line'].create({
-                    'product_id': product.id,
+                    'product_id': line.product_id.id,
                     'name': product_name,
                     'date_planned': order_id.date_planned or datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT),
-                    'product_uom': product.uom_po_id.id if product.uom_po_id else product.uom_id.id,
-                    'price_unit': product.lst_price,
-                    'product_qty': 1.0,
+                    'product_uom': line.product_id.uom_po_id.id if line.product_id.uom_po_id else line.product_id.uom_id.id,
+                    'price_unit': line.product_id.lst_price,
+                    'product_qty': line.product_qty,
                     'display_type': False,
                     'order_id': order_id.id
                 })
+
+
+class SelectProductsLine(models.TransientModel):
+    _name = 'select.products.line'
+    _description = 'Select Products Line'
+
+    product_id = fields.Many2one('product.product', string='Product', required=True)
+    product_qty = fields.Float(string='Quantity', default=1.0, required=True)
+    wizard_id = fields.Many2one('select.products', string='Wizard')
